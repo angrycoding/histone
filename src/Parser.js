52731e0d43d81d12ff54b319f14531aee169fc42
Ruslan <matveev.ruslan@gmail.com>
2025-09-04 11:04:48 +0200

    Update Parser.js

--- src/Parser.js
+++ src/Parser.js
@@ -1,10 +1,13 @@
-var Utils = require('./Utils'),
-	Constants = require('./Constants'),
+import Utils from './Utils.js';
+import Constants from './Constants.js';
+import ParserImpl from './parser/Parser.js';
+
+var
 	AST_UNDEFINED = Constants.AST_UNDEFINED,
 	TPL_AST_REGEXP = /^\s*\[\s*[0-9]+.*\]\s*$/,
-	Parser = Constants.HAS_PARSER && require('./parser/Parser');
+	Parser = Constants.HAS_PARSER && ParserImpl;
 
-module.exports = function(template, baseURI) {
+export default function(template, baseURI) {
 	if (Utils.$isString(template)) {
 		if (TPL_AST_REGEXP.test(template)) try {
 			return JSON.parse(template);
